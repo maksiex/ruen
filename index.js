0f79e9d8ac8d918c7970ff5877f65bf403f1297e
nekroot@mail.ru <nekroot@mail.ru>
2024-01-09 20:21:05 +0300

    Added words

---
+++ index.js
@@ -0,0 +1,40 @@
+const words = require('./words.js');
+const readline = require('readline');
+const colors = require('colors/safe');
+
+function getRandomWords(num) {
+    const randomWords = [];
+    for (let i = 0; i < num; i++) {
+        const randomIndex = Math.floor(Math.random() * words.length);
+        randomWords.push(words[randomIndex]);
+    }
+    return randomWords;
+}
+
+function startGame(lang) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const randomWords = getRandomWords(3);
+    let currentIndex = 0;
+
+    console.log(`${colors.magenta('Переведите слово')} ${colors.bold(randomWords[currentIndex][lang])} ${colors.underline('на другой язык:')} `);
+
+    rl.on('line', (input) => {
+        if (input.toLowerCase() === randomWords[currentIndex][lang === 'en' ? 'ru' : 'en'].toLowerCase()) {
+            currentIndex++;
+            if (currentIndex < randomWords.length) {
+                console.log(`${colors.bold(colors.green('Правильно! Следующее слово:'))} ${colors.underline(randomWords[currentIndex][lang])}`);
+            } else {
+                console.log(colors.bgWhite(colors.grey(colors.italic(colors.bold('Игра окончена.')))));
+                rl.close();
+            }
+        } else {
+            console.log(`${colors.yellow(colors.bold('Неверный перевод. Попробуйте еще раз перевести'))} ${randomWords[currentIndex][lang]} на другой язык: `);
+        }
+    });
+}
+
+startGame('en');
